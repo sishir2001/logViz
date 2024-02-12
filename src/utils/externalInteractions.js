960daf4512c7d6d651cb4e7bf548b0d615b2875e
jaladi.sishir <jaladi.sishir@juspay.in>
2024-02-12 20:24:34 +0530

    refactor main.js

---
+++ src/utils/externalInteractions.js
@@ -0,0 +1,46 @@
+const fs = require("fs").promises;
+const { exec } = require("child_process");
+
+async function readFileAsync(filePath) {
+  try {
+    const data = await fs.readFile(filePath, "utf8");
+    return data;
+  } catch (error) {
+    console.error(`Got an error trying to read the file: ${error.message}`);
+  }
+}
+
+async function writeFileAsync(filePath, data) {
+  try {
+    await fs.writeFile(filePath, data);
+  } catch (error) {
+    console.error(`Got an error trying to write to a file: ${error.message}`);
+  }
+}
+
+async function createDirectory(path, isRecursive = false) {
+  try {
+    await fs.mkdir(path, { recursive: isRecursive });
+  } catch (error) {
+    console.error(
+      `Got an error trying to create a directory: ${error.message}`
+    );
+  }
+}
+
+function executeCommand(command){
+  exec(command,(error,stdout,stderr)=>{
+    if(error){
+      console.error(`error: ${error.message}`);
+      return;
+    }
+    if(stderr){
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  })
+}
+
+
+module.exports = { readFileAsync, writeFileAsync, createDirectory,executeCommand };
